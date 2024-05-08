86e223dcf5700f6a15c7d598520ecea66e38d216
“Thai <31430293+minhthai1995@users.noreply.github.com>
2024-05-08 14:06:45 +1000

    update percentage

--- public/app.js
+++ public/app.js
@@ -54,17 +54,17 @@ function displayData(data) {
 
     data.forEach(account => {
         const row = document.createElement('tr');
-        const addressCell = document.createElement('td');
-        addressCell.textContent = account.address;
-        const amountCell = document.createElement('td');
-        amountCell.textContent = account.amount;
+        // const addressCell = document.createElement('td');
+        // addressCell.textContent = account.address;
+        // const amountCell = document.createElement('td');
+        // amountCell.textContent = account.amount;
         const ownerCell = document.createElement('td');
         ownerCell.textContent = account.owner;
         const percentageCell = document.createElement('td');
         percentageCell.textContent = account.percentage;
 
-        row.appendChild(addressCell);
-        row.appendChild(amountCell);
+        // row.appendChild(addressCell);
+        // row.appendChild(amountCell);
         row.appendChild(ownerCell);
         row.appendChild(percentageCell);
         tableBody.appendChild(row);
